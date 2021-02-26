91fb7f15aa7bc0ff7a2ddceef1b8293c6118c57b
Joey Steigelman <jhs430@nyu.edu>
2021-02-26 10:22:28 +0100

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Joey Steigelman on 2/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
