fc6f10fa2b4b39108540ac473f42c88aff74df77
Joey Steigelman <jhs430@nyu.edu>
2021-02-26 20:59:05 +0100

    Added party size segmented control and functionality; improved UI

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,21 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
     @IBOutlet weak var billAmountTextField: UITextField!
     @IBOutlet weak var tipAmountLabel: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var perPersonLabel: UILabel!
+    @IBOutlet var cardsCollection: [UIView]!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var partySizeControl: UISegmentedControl!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        configureCardsCollection()
     }
-
+    
+    func configureCardsCollection() {
+        for card in cardsCollection {
+            //add rounded corners to cards
+            card.layer.cornerRadius = 20
+        }
+    }
+    
     @IBAction func calculateTip(_ sender: Any) {
-        
         //get bill amount from text field input
         let bill = Double(billAmountTextField.text!) ?? 0
         
@@ -39,7 +47,32 @@ class ViewController: UIViewController {
         //update total amount
         totalLabel.text = String(format: "$%.2f", total)
         
+        //calculate per person amount
+        let partySize = [1, 2, 3, 4, 5, 6, 7, 8]
+        let selectedPartySize = partySize[partySizeControl.selectedSegmentIndex]
+        let perPerson = total / (Double(selectedPartySize))
+        
+        //update per person amount
+        perPersonLabel.text = String(format: "$%.2f", perPerson)
+        
     }
+    
+    
+    @IBAction func calculatePerPerson(_ sender: Any) {
+        //call calculateTip to update per person amount
+        calculateTip(UIButton())
+    }
+    
+    //calculate tip when Bill Amount is edited
+    @IBAction func textFieldChanged(_ sender: UITextField) {
+        calculateTip(UIButton())
+    }
+    
+    //Dismiss Numpad when tap outside of numpad
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
     
     
 }
